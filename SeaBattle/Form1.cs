0afcdc8d81ceb1d29c5df86a6041094ef6ca20f1
Akylbenko <licenzeeya@gmail.com>
2025-12-07 23:44:43 +1000

    add hosting and connecting

--- SeaBattle/Form1.cs
+++ SeaBattle/Form1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace SeaBattle
 {
@@ -16,13 +15,24 @@ namespace SeaBattle
         public const int cellSize = 30;
         public string alphabet = "АБВГДЕЖЗИК";
 
-        public int[,] myMap = new int[mapSize, cellSize];
-        public int[,] enemyMap = new int[mapSize, cellSize];
+        public int[,] myMap = new int[mapSize, mapSize];
+        public int[,] enemyMap = new int[mapSize, mapSize];
 
-        public Button[,] myButtons = new Button[mapSize, cellSize];
-        public Button[,] enemyButtons = new Button[mapSize, cellSize];
+        public Button[,] myButtons = new Button[mapSize, mapSize];
+        public Button[,] enemyButtons = new Button[mapSize, mapSize];
 
-        public bool isPlaying = false;
+        TcpClient client;
+        TcpListener server;
+        NetworkStream stream;
+
+        bool isMyTurn = false;
+        bool connected = false;
+
+        TextBox ipBox;
+        TextBox portBox;
+        Button hostButton;
+        Button joinButton;
+        Label statusLabel;
 
         public Form1()
         {
@@ -33,40 +43,64 @@ namespace SeaBattle
 
         public void Init()
         {
+            CreateGUI();
             CreateMaps();
+        }
+
+        private void CreateGUI()
+        {
+            ipBox = new TextBox();
+            ipBox.Location = new Point(10, mapSize * cellSize + 40);
+            ipBox.Text = "127.0.0.1";
+            this.Controls.Add(ipBox);
+
+            portBox = new TextBox();
+            portBox.Location = new Point(120, mapSize * cellSize + 40);
+            portBox.Text = "9000";
+            this.Controls.Add(portBox);
+
+            hostButton = new Button();
+            hostButton.Text = "Host";
+            hostButton.Location = new Point(10, mapSize * cellSize + 70);
+            hostButton.Click += HostGame;
+            this.Controls.Add(hostButton);
+
+            joinButton = new Button();
+            joinButton.Text = "Join";
+            joinButton.Location = new Point(100, mapSize * cellSize + 70);
+            joinButton.Click += JoinGame;
+            this.Controls.Add(joinButton);
+
+            statusLabel = new Label();
+            statusLabel.Location = new Point(10, mapSize * cellSize + 110);
+            statusLabel.Size = new Size(300, 25);
+            statusLabel.Text = "Не подключено";
+            this.Controls.Add(statusLabel);
         }
 
         public void CreateMaps()
         {
             this.Width = mapSize * 2 * cellSize + 70;
-            this.Height = mapSize * cellSize + 170;
+            this.Height = mapSize * cellSize + 200;
+
             for (int i = 0; i < mapSize; i++)
             {
                 for (int j = 0; j < mapSize; j++)
                 {
                     myMap[i, j] = 0;
 
-                    Button button = new Button();
-                    button.Location = new Point(j * cellSize, i * cellSize);
-                    button.Size = new Size(cellSize, cellSize);
-                    this.Controls.Add(button);
-                    myButtons[i, j] = button;
+                    Button b = new Button();
+                    b.Location = new Point(j * cellSize, i * cellSize);
+                    b.Size = new Size(cellSize, cellSize);
+                    this.Controls.Add(b);
+                    myButtons[i, j] = b;
+
                     if (i == 0 || j == 0)
                     {
-                        button.BackColor = Color.Gray;
-                        if (i == 0 && j > 0)
-                        {
-                            button.Text = alphabet[j - 1].ToString();
-                        }
-                        if (j == 0 && i > 0)
-                        {
-                            button.Text = i.ToString();
-                        }
-                    } else
-                    {
-                        button.Click += ConfigureShips;
+                        b.BackColor = Color.Gray;
+                        if (i == 0 && j > 0) b.Text = alphabet[j - 1].ToString();
+                        if (j == 0 && i > 0) b.Text = i.ToString();
                     }
-
                 }
             }
 
@@ -76,99 +110,127 @@ namespace SeaBattle
                 {
                     enemyMap[i, j] = 0;
 
-                    Button button = new Button();
-                    button.Location = new Point(320 + j * cellSize, i * cellSize);
-                    button.Size = new Size(cellSize, cellSize);
-                    this.Controls.Add(button);
-                    enemyButtons[i, j] = button;
+                    Button b = new Button();
+                    b.Location = new Point(350 + j * cellSize, i * cellSize);
+                    b.Size = new Size(cellSize, cellSize);
+                    this.Controls.Add(b);
+                    enemyButtons[i, j] = b;
+
                     if (i == 0 || j == 0)
                     {
-                        button.BackColor = Color.Gray;
-                        if (i == 0 && j > 0)
-                        {
-                            button.Text = alphabet[j - 1].ToString();
-                        }
-                        if (j == 0 && i > 0)
-                        {
-                            button.Text = i.ToString();
-                        }
-                    } else
+                        b.BackColor = Color.Gray;
+                        if (i == 0 && j > 0) b.Text = alphabet[j - 1].ToString();
+                        if (j == 0 && i > 0) b.Text = i.ToString();
+                    }
+                    else
                     {
-                        button.Click += PlayerShoot;
+                        b.Click += PlayerShoot;
                     }
                 }
             }
-
-            Label myMapLabel = new Label();
-            myMapLabel.Location = new Point(mapSize * cellSize / 2 - 30, mapSize * cellSize + 10);
-            myMapLabel.Text = "Карта игрока";
-            this.Controls.Add(myMapLabel);
-
-            Label enemyMapLabel = new Label();
-            enemyMapLabel.Location = new Point(mapSize * cellSize / 2 + 290, mapSize * cellSize + 10);
-            enemyMapLabel.Text = "Карта противника";
-            this.Controls.Add(enemyMapLabel);
-
-            Button startButton = new Button();
-            startButton.Location = new Point(0, mapSize * cellSize + 30);
-            startButton.Size = new Size(120, 30);
-            startButton.Text = "Играть!";
-            startButton.Click += Start;
-            this.Controls.Add(startButton);
         }
 
-        public void Start(object sender, EventArgs e)
+        private async void HostGame(object sender, EventArgs e)
         {
-            isPlaying = true;
+            int port = int.Parse(portBox.Text);
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            statusLabel.Text = "Ожидание подключения...";
+            client = await server.AcceptTcpClientAsync();
+            stream = client.GetStream();
+            connected = true;
+
+            isMyTurn = true;
+            statusLabel.Text = "Игрок подключён. Ваш ход.";
+
+            StartListening();
         }
 
-        public void ConfigureShips(object sender, EventArgs e)
-        {   
-            Button pressedButton = sender as Button;
-            if (!isPlaying)
+        private async void JoinGame(object sender, EventArgs e)
+        {
+            try
             {
-                if (myMap[pressedButton.Location.X / cellSize, pressedButton.Location.Y / cellSize] == 0)
-                {
-                    pressedButton.BackColor = Color.Red;
-                    myMap[pressedButton.Location.X / cellSize, pressedButton.Location.Y / cellSize] = 1;
-                } else
-                {
-                    pressedButton.BackColor = Color.White;
-                    myMap[pressedButton.Location.X / cellSize, pressedButton.Location.Y / cellSize] = 0;
-                }
+                client = new TcpClient();
+                await client.ConnectAsync(ipBox.Text, int.Parse(portBox.Text));
+                stream = client.GetStream();
+                connected = true;
+
+                isMyTurn = false;
+                statusLabel.Text = "Подключено. Ход противника.";
+
+                StartListening();
+            }
+            catch
+            {
+                statusLabel.Text = "Ошибка подключения";
             }
         }
 
-        public void PlayerShoot(object sender, EventArgs e)
+        private async void StartListening()
         {
-            Button pressedButton = sender as Button;
-            Shoot(enemyMap, pressedButton);
+            await Task.Run(async () =>
+            {
+                byte[] buffer = new byte[1024];
+
+                while (connected)
+                {
+                    int bytes = 0;
+                    try
+                    {
+                        bytes = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    }
+                    catch { break; }
+
+                    if (bytes == 0) break;
+
+                    string json = Encoding.UTF8.GetString(buffer, 0, bytes);
+                    var msg = JsonConvert.DeserializeObject<Message>(json);
+
+                    if (msg.Type == "shot")
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            HandleIncomingShot(msg.X, msg.Y);
+                        }));
+                    }
+                }
+            });
         }
 
-        public bool Shoot(int[,] map, Button pressedButton)
+        private void PlayerShoot(object sender, EventArgs e)
         {
-            bool hit = false;
-            if (isPlaying)
-            {   
-                int delta = 0;
-                if (pressedButton.Location.X > 320)
-                {
-                    delta = 320;
-                }
-                if (map[(pressedButton.Location.X - delta) / cellSize, pressedButton.Location.Y / cellSize] != 0)
-                {
-                    hit = true;
+            if (!isMyTurn || !connected) return;
 
-                    pressedButton.BackColor = Color.Blue;
-                    pressedButton.Text = "X";
-                }
-                else
-                {
-                    hit = false;
-                    pressedButton.BackColor = Color.Black;
-                }
-            }
-            return hit;
+            Button b = sender as Button;
+
+            int x = (b.Location.X - 350) / cellSize;
+            int y = b.Location.Y / cellSize;
+
+            SendMessage(new Message { Type = "shot", X = x, Y = y });
+            isMyTurn = false;
+            statusLabel.Text = "Ход противника";
+        }
+
+        private void HandleIncomingShot(int x, int y)
+        {
+            myButtons[y, x].BackColor = Color.Black;
+            isMyTurn = true;
+            statusLabel.Text = "Ваш ход";
+        }
+
+        public class Message
+        {
+            public string Type;
+            public int X;
+            public int Y;
+        }
+
+        private void SendMessage(Message msg)
+        {
+            string json = JsonConvert.SerializeObject(msg);
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            stream.Write(data, 0, data.Length);
         }
     }
 }
